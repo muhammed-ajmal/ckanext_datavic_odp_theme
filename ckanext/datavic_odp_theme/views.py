05b6cfdbbc7d56a23466ce699e1a3a1da0640b2f
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2023-01-30 21:07:29 +0200

    SXDEDPCXZIC-105_DATAVIC-499 / add DTV feature

--- ckanext/datavic_odp_theme/views.py
+++ ckanext/datavic_odp_theme/views.py
@@ -1,12 +1,82 @@
-from flask import Blueprint
+from __future__ import annotations
+
+from flask import Blueprint, jsonify
 
 import ckan.lib.helpers as h
 
 vic_odp = Blueprint('vic_odp', __name__)
+CONFIG_BASE_MAP = "ckanext.datavicmain.dtv.base_map_id"
+DEFAULT_BASE_MAP = "vic-topographic"
 
 
 def vic_groups_list(id):
     return h.redirect_to('dataset.read', id=id)
 
 
+def dtv_config():
+    embedded = toolkit.asbool(toolkit.request.args.get("embedded"))
+    ids: list[str] = toolkit.request.args.getlist("resource_id")
+    base_url: str = (
+        toolkit.config.get("ckanext.datavicmain.odp.public_url")
+        or toolkit.config["ckan.site_url"]
+    )
+
+    catalog = []
+    pkg_cache = {}
+
+    for id_ in ids:
+
+        try:
+            resource = toolkit.get_action("resource_show")({}, {"id": id_})
+            if resource["package_id"] not in pkg_cache:
+                pkg_cache[resource["package_id"]] = toolkit.get_action("package_show")(
+                    {}, {"id": resource["package_id"]}
+                )
+
+        except (toolkit.NotAuthorized, toolkit.ObjectNotFound):
+            continue
+
+        pkg = pkg_cache[resource["package_id"]]
+        catalog.append({
+            "id": f"data-vic-embed-{id_}",
+            "name": "{}: {}".format(
+                pkg["title"],
+                resource["name"] or "Unnamed"
+            ),
+            "type": "ckan-item",
+            "url": base_url,
+            "resourceId": id_
+        })
+
+    return jsonify({
+        "version": "8.0.0",
+        "initSources": [{
+            "baseMaps": {
+                "defaultBaseMapId": toolkit.config.get(
+                    CONFIG_BASE_MAP, DEFAULT_BASE_MAP
+                )
+            },
+            "catalog": catalog,
+            "workbench": [item["id"] for item in catalog],
+            "elements": {
+                "map-navigation": {
+                    "disabled": embedded
+                },
+                "menu-bar": {
+                    "disabled": embedded
+                },
+                "bottom-dock": {
+                    "disabled": embedded
+                },
+                "map-data-count": {
+                    "disabled": embedded
+                },
+                "show-workbench": {
+                    "disabled": embedded
+                }
+            }
+        }]
+    })
+
 vic_odp.add_url_rule( u'/dataset/groups/<id>', view_func=vic_groups_list)
+vic_odp.add_url_rule('/dtv_config', view_func=dtv_config)
