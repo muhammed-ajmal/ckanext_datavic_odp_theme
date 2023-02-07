3442923c8a865a3b9cac71da69bbf16fa27ce2d1
ajmal <dev@ajmalaju.com>
2023-02-07 10:24:46 +0530

    SXDEDPCXZIC-112/remove activity stream from org for non auth users

--- ckanext/datavic_odp_theme/logic/__init__.py
+++ ckanext/datavic_odp_theme/logic/__init__.py
@@ -4,5 +4,6 @@ from .auth import get
 def auth_functions():
     return dict(
         acitvity_list=get.vic_activity_list,
-        package_activity_list=get.vic_package_activity_list
+        package_activity_list=get.vic_package_activity_list,
+        organization_activity_list = get.vic_organization_activity_list
     )
